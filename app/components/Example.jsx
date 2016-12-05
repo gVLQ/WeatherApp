724170337d6834c1e17a7f5338ee51abf5683e96
Gabriel Vlaicu <vlaicu.gabriel11@yahoo.com>
2016-12-05 22:02:26 +0200

    style pagina About

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,11 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Example = React.createClass({
   render: function () {
     return (
       <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Exemple de cautari:</p>
+      <ol>
+        <li><Link to='/?location=Bucharest'>Bucharest, RO</Link></li>
+        <li><Link to='/?location=NewYork'>New York, US</Link></li>
+      </ol>
       </div>
     )
   }
